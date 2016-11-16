414a6f778db8fae7e7da112a216ff19914943976
lujjjh <lujjjh@gmail.com>
2016-11-16 16:08:05 +0800

    Append hash to filename

--- faas/install_production.sh
+++ faas/install_production.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 grep -Pzo "<script extract>\s*\K([\s\S]+?)(?=\s*</script>)" index.html | tr '\0' '\n' > index.js
-sed -i '/<script extract>/{:b;$!N;/<\/script>/!bb;s/.*/<script src="https:\/\/faas.elemecdn.com\/restc\/index.js"><\/script>/}' index.html
+hash=$(md5sum index.js | cut -c 1-5)
+mv index.js index-$hash.js
+sed -i "/<script extract>/{:b;$!N;/<\/script>/!bb;s/.*/<script src=\"https:\/\/faas.elemecdn.com\/restc\/index-$hash.js\"><\/script>/}" index.html
