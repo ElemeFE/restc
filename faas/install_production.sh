a1e14bf8ec48cee3d44cdd196e51ca2af4c33655
lujjjh <lujjjh@gmail.com>
2016-11-25 23:59:30 +0800

    Fix grep arguments in production installation hook

--- faas/install_production.sh
+++ faas/install_production.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-grep -Pzo "<script extract>\s*\K([\s\S]+?)(?=\s*</script>)" index.html | tr '\0' '\n' > index.js
+grep --text -Pzo "<script extract>\s*\K([\s\S]+?)(?=\s*</script>)" index.html | tr '\0' '\n' > index.js
 hash=$(md5sum index.js | cut -c 1-5)
 mv index.js index-$hash.js
 sed -i "/<script extract>/{:b;$!N;/<\/script>/!bb;s/.*/<script src=\"https:\/\/faas.elemecdn.com\/restc\/index-$hash.js\"><\/script>/}" index.html
