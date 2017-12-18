f4d7f59d92e9045e7abf01b62234509f3119c0d8
lujjjh <lujjjh@gmail.com>
2017-12-18 14:54:02 +0800

    Migrate FaaS CDN to Shadow

--- faas/install_production.sh
+++ faas/install_production.sh
@@ -3,4 +3,4 @@
 grep --text -Pzo "<script extract>\s*\K([\s\S]+?)(?=\s*</script>)" index.html | tr '\0' '\n' > index.js
 hash=$(md5sum index.js | cut -c 1-5)
 mv index.js index-$hash.js
-sed -i "/<script extract>/{:b;$!N;/<\/script>/!bb;s/.*/<script src=\"https:\/\/faas.elemecdn.com\/restc\/index-$hash.js\"><\/script>/}" index.html
+sed -i "/<script extract>/{:b;$!N;/<\/script>/!bb;s/.*/<script src=\"https:\/\/shadow.elemecdn.com\/faas\/restc\/index-$hash.js\"><\/script>/}" index.html
